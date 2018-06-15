efeb17ed7fbc137f9d7981a280755ebbd9041cd9
Hammer <jgwmaxwell@gmail.com>
2018-06-15 14:19:38 +0100

    revert to grpclog

--- server/server.go
+++ server/server.go
@@ -10,6 +10,8 @@ import (
 
 	"github.com/musicglue/httpgrpc"
 	"github.com/musicglue/httpgrpc/utils"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // Server implements HTTPServer.  HTTPServer is a generated interface that gRPC
@@ -34,15 +36,23 @@ func (s Server) Handle(ctx context.Context, r *httpgrpc.HTTPRequest) (*httpgrpc.
 	req = req.WithContext(ctx)
 	utils.ToHeader(r.Headers, req.Header)
 	req.RequestURI = r.Url
+
+	for k, v := range req.Header {
+		log.Infof("received header: %s -> %s", k, v)
+	}
+
 	recorder := httptest.NewRecorder()
 	s.handler.ServeHTTP(recorder, req)
+
 	resp := &httpgrpc.HTTPResponse{
 		Code:    int32(recorder.Code),
 		Headers: utils.FromHeader(recorder.Header()),
 		Body:    recorder.Body.Bytes(),
 	}
+
 	if recorder.Code/100 == 5 {
 		return nil, httpgrpc.ErrorFromHTTPResponse(resp)
 	}
+
 	return resp, err
 }
