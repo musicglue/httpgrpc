7f2f44604196df640cbf7bf1e20875b81c838a6f
Hammer <jgwmaxwell@gmail.com>
2018-06-15 12:46:01 +0100

    clear hop headers

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,26 @@ import (
 	"github.com/musicglue/httpgrpc"
 )
 
+var hopHeaders = []string{
+	"Connection",
+	"Proxy-Connection", // non-standard but still sent by libcurl and rejected by e.g. google
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Te",      // canonicalized version of "TE"
+	"Trailer", // not Trailers per URL above; http://www.rfc-editor.org/errata_search.php?eid=4522
+	"Transfer-Encoding",
+	"Upgrade",
+}
+
+var skipHopHeader = make(map[string]bool, len(hopHeaders))
+
+func init() {
+	for _, v := range hopHeaders {
+		skipHopHeader[v] = true
+	}
+}
+
 // ToHeader turns a grpc header to a http header
 func ToHeader(hs []*httpgrpc.Header, header http.Header) {
 	for _, h := range hs {
@@ -17,6 +37,10 @@ func ToHeader(hs []*httpgrpc.Header, header http.Header) {
 func FromHeader(hs http.Header) []*httpgrpc.Header {
 	result := make([]*httpgrpc.Header, 0, len(hs))
 	for k, vs := range hs {
+		if skipHopHeader[k] {
+			continue
+		}
+
 		result = append(result, &httpgrpc.Header{
 			Key:    k,
 			Values: vs,
